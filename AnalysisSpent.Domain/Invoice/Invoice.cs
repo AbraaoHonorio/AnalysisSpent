70ffdd547cc7f1b25d13a8715fbd96e4582997ed
Abraão <abraao@socialminer.com>
2019-09-02 23:11:19 -0300

    adicionando o dominio da nossa aplicação

---
+++ AnalysisSpent.Domain/Invoice/Invoice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnalysisSpent.Domain.Invoice
+{
+    public class Invoice
+    {
+        public DateTime Date { get; private set; }
+        public string Describe { get; private set; }
+        public decimal Value { get; private set; }
+        public string Category { get; private set; }
+
+        public Invoice SetData(string data)
+        {
+            Date = DateTime.Parse(data);
+            return this;
+        }
+
+        public Invoice SetDescricao(string descricao)
+        {
+            Describe = descricao;
+            return this;
+        }
+
+        public Invoice SetValor(string valor)
+        {
+            Value = Convert.ToDecimal(valor);
+            return this;
+        }
+
+        public Invoice SetCategoria(string categoria)
+        {
+            Category = categoria;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return $"Data Descricao Valor Categoria" +
+                   $"{Date} {Describe} {Value} {Category}";
+        }
+    }
+}
